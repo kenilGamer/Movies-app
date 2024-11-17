180fffd2404898bb6682a3fbdae4d389c67c45f2
kenil Gamer <kenilk677@gmail.com>
2024-11-17 14:06:16 +0530

    sds

--- src/components/Flashmessage.jsx
+++ src/components/Flashmessage.jsx
@@ -14,7 +14,7 @@ function Flashmessage({ getError, errorKey, duration = 5000 }) {
     return (
         visible && (
             <div
-                className="absolute top-2 max-md:left-1/2 transform max-md:-translate-x-1/2 right-2 text-[#D32F2F] text-center text-xl font-bold p-4 rounded-xl shadow-md transition-opacity duration-1000 opacity-100 hover:opacity-80"
+                className="absolute top-2 max-md:left-1/2 transform max-md:-translate-x-1/2 right-2 text-[#D32F2F] text-center text-xl font-bold p-4 rounded-xl shadow-md transition-opacity duration-1000 opacity-100 hover:opacity-80 max-md:w-[90vw] max-md:text-sm"
                 style={{
                     background: 'linear-gradient(135deg, #FFCDD2, #FFEBEE)',
                     maxWidth: '90%',
