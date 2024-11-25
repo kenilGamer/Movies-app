e72933ffcee88433c0f30478523cb15a73b48e8e
kenil Gamer <kenilk677@gmail.com>
2024-11-25 13:28:22 +0530

    SDWQS

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    
     rollupOptions: {
-      external: ['react-toastify'],
+      external: ['react-toastify/dist/ReactToastify.css'], // Explicitly externalize the CSS
     },
   },
 });
