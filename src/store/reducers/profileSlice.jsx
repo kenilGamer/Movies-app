ccfb767bc16c153f4fc718b280a8142927244a49
kenil Gamer <kenilk677@gmail.com>
2024-12-10 12:07:50 +0530

    v

--- src/store/reducers/profileSlice.jsx
+++ src/store/reducers/profileSlice.jsx
@@ -7,15 +7,28 @@ const profileSlice = createSlice({
     error: null,
   },
   reducers: {
+    /**
+     * Sets the profile in the state to the given payload or clears the profile if the
+     * payload is null. Clears the error if the payload is not null.
+     *
+     * @param {Object} state - The current state of the store.
+     * @param {Object} action - The action containing the payload to use to set the profile.
+     * @param {Object|null} action.payload - The profile data to set or null to clear the profile.
+     */
     setProfile: (state, action) => {
+      if (!action.payload) {
+        state.profile = null;
+        state.error = null;
+        return;
+      }
       state.profile = action.payload;
       state.error = null;
     },
-    setError: (state, action) => {
-      console.log(action.payload);
-      localStorage.removeItem("token");
+    setError: (state, { payload: error }) => {
       state.profile = null;
+      state.error = error;
       state.error = action.payload;
+      localStorage.removeItem("token");
     },
   },
 });
