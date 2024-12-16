a7f8aa4d051d82faff9c34a5d5edd67c927b16f2
kenil Gamer <kenilk677@gmail.com>
2024-12-16 22:18:31 +0530

    qwedqrd

--- src/partials/Trailer.jsx
+++ src/partials/Trailer.jsx
@@ -11,20 +11,24 @@ function Trailer() {
   const { pathname } = useLocation();
   const category = pathname.includes("movie") ? "movie" : "tv";
   const ytvideos = useSelector((state) => state[category].info.videos);
-  console.log(ytvideos);
+  
+  console.log(ytvideos.key);
   return (
     <div className="absolute overflow-hidden w-screen h-screen flex items-center justify-center bg-[#000000f5] top-0 left-0 ">
       <Link
         onClick={() => navigate(-1)}
         className="hover:text-[#6556CD] absolute text-2xl top-[5%] right-[5%] ri-close-fill"
       ></Link>
-    {ytvideos ? <ReactPlayer
-        controls={true}
-        url={`https://www.youtube.com/watch?v=${ytvideos.key}`}
-        // height={500}
-        // width={1100}
-        className="w-full"
-      />:(
+    {ytvideos ?  
+    // <ReactPlayer
+    //     controls={true}
+    //     url={`https://www.youtube.com/watch?v=${ytvideos.key}`}
+    //     // height={500}
+    //     // width={1100}
+    //     className="w-full"
+    //   />
+    <iframe src={`https://www.youtube.com/embed/${ytvideos.key}`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" frameborder="0" allowfullscreen className="w-full h-full m-2"></iframe>
+      :(
         <NotFound/>
       )}
     
